e96c0e6efb7cfdd0d3c35157c85d11679f322a98
Honey Agrawal <agrawalhoney978@gmail.com>
2026-05-29 20:44:09 +0530

    DAY2

---
+++ DAY2/question3.java
@@ -0,0 +1,18 @@
+package DAY2;
+import java.util.Scanner;
+public class question3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your number : ");
+        int n = sc.nextInt();
+        int product = 1;
+        while(n != 0){
+            int digit = n % 10;
+            product = product * digit;
+            n = n / 10;
+        }
+
+        System.out.println("Product of given number is : "+ product);
+        sc.close();
+    }
+}
