6b2b66ebe9761e73557f126061777766e2024f2f
Honey Agrawal <agrawalhoney978@gmail.com>
2026-05-30 17:15:15 +0530

    DAY3

--- DAY2/question5.java
+++ DAY2/question5.java
@@ -1,7 +1,7 @@
 package DAY2;
 import java.util.Scanner;
 
-public class question1 {
+public class question5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int sum = 0;
