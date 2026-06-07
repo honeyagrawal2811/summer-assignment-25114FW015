75485504c9f1f2c891f78124c68bc34f2af83571
Honey Agrawal <agrawalhoney978@gmail.com>
2026-06-07 16:18:24 +0530

    DAY10

---
+++ DAY10/question37.java
@@ -0,0 +1,22 @@
+package DAY10;
+import java.util.Scanner;
+public class question37 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int n=5;
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=(n-i);j++) {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++) {
+                System.out.print("*");
+            }
+            for(int j=2;j<=i;j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+    
+}
---
+++ DAY10/question38.java
@@ -0,0 +1,22 @@
+package DAY10;
+import java.util.Scanner;
+public class question38 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=5;
+        for(int i=n;i>=1;i--) {
+            for(int j=1;j<=(n-i);j++) {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++) {
+                System.out.print("*");
+            }
+            for(int j=2;j<=i;j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+    
+}
