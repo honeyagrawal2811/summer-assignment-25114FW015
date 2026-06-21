8782c835a8567a75218bc4a7c5a478602cd4df73
Honey Agrawal <agrawalhoney978@gmail.com>
2026-06-21 22:25:51 +0530

    DAY21

---
+++ DAY21/question81.java
@@ -0,0 +1,15 @@
+package DAY21;
+import java.util.Scanner;
+public class question81 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        int count = 0;
+        for (char ch : str.toCharArray()) {
+            count++;
+        }
+        System.out.println("Length of string = " + count);
+        sc.close();
+    }
+}
---
+++ DAY21/question83.java
@@ -0,0 +1,5 @@
+package DAY21;
+
+public question8 {
+    
+}
